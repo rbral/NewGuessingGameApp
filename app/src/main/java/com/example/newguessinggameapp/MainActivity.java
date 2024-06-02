01fb80112f37714836b1f6645043f34bb951f18e
rbral <rbral@student.touro.edu>
2024-06-02 15:10:09 -0400

    added launcher, fixed bugs, project is complete

--- app/src/main/java/com/example/newguessinggameapp/MainActivity.java
+++ app/src/main/java/com/example/newguessinggameapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -15,28 +16,65 @@ import com.example.newguessinggameapp.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
+    private GuessingGame mGame;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("GAME", mGame.getJSONStringFromThis());
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action",
-                                Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView();
+        setSupportActionBar(binding.toolbar);
+        setupButtonListeners();
+        binding.fab.setOnClickListener(view -> mGame.startNewGame());
+        mGame = savedInstanceState != null
+                ? GuessingGame.getGuessingGameObjectFromJSONString(
+                savedInstanceState.getString("GAME"))
+                : new GuessingGame();
     }
 
+    private void setContentView() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
 
+    private void setupButtonListeners() {
+        findViewById(R.id.button1).setOnClickListener(this::handleButton123Click);
+        findViewById(R.id.button2).setOnClickListener(this::handleButton123Click);
+        findViewById(R.id.button3).setOnClickListener(this::handleButton123Click);
+
+        // or, set ids in content_main, etc. and use ViewBinding instead of findViewById above.
+        // and/or, if you had more than two or three buttons, you could use an array and loop...
+/*
+        Button [] buttons = {
+                findViewById(R.id.button1), findViewById(R.id.button2), findViewById(R.id.button3)};
+
+        for (Button currentButton : buttons) {
+            currentButton.setOnClickListener(this::handleButton123Click);
+        }
+*/
+    }
+
+    public void handleButton123Click(View view) {
+        Button currentButton = (Button) view;
+        String currentButtonText = currentButton.getText().toString();
+        String resultsText = currentButtonText.equals(String.valueOf(mGame.getWinningNumber()))
+                ? String.format(Locale.US, "Yes! %s is the right number.", currentButtonText)
+                : String.format(Locale.US, "Sorry. %s is not the right number.", currentButtonText);
+
+        Snackbar.make(view, resultsText, Snackbar.LENGTH_SHORT).show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,6 +97,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
 }
